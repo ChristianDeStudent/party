d14ab868079fa87b17613759a87a2110736282aa
Christian Mathias Vollstedt <Christianvollstedt1@outlook.com>
2026-02-15 23:00:29 +0100

    16de: Ik had wat data gezet bij de database over Client.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT into Venue (venue_name, link_more_info, capacity, food_provided, indoor, outdoor, free_parking_available, distance_from_public_transport_in_km)
 VALUES ('Jada events', 'https://jada-events.be/',150,true, true,false, true,2);
+
+INSERT INTO Client (name, nr_Of_Orders, total_amount, discount_taken)
+VALUES ('John', 5,23,4.99);
