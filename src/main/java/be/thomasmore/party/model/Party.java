bb35f84d8e0cdd03063d044d438201220203852c
Christian Mathias Vollstedt <Christianvollstedt1@outlook.com>
2026-03-02 20:42:22 +0100

    53ste:IK heb getters en setters gemaakt en ook de Venue venue in de party class gemaakt en het many to one gemaakt :)

--- src/main/java/be/thomasmore/party/model/Party.java
+++ src/main/java/be/thomasmore/party/model/Party.java
@@ -1,9 +1,6 @@
 package be.thomasmore.party.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalTime;
 
@@ -19,4 +16,79 @@ public class Party {
     private String extraInfo;
     private LocalTime date,doors;
     private String imageUrl;
+    @ManyToOne
+    @JoinColumn(name = "venue_id")
+    private Venue venue;
+
+    public Venue getVenue() {
+        return venue;
+    }
+
+    public void setVenue(Venue venue) {
+        this.venue = venue;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPricePresaleInEur() {
+        return pricePresaleInEur;
+    }
+
+    public void setPricePresaleInEur(int pricePresaleInEur) {
+        this.pricePresaleInEur = pricePresaleInEur;
+    }
+
+    public int getPriceInEur() {
+        return priceInEur;
+    }
+
+    public void setPriceInEur(int priceInEur) {
+        this.priceInEur = priceInEur;
+    }
+
+    public String getExtraInfo() {
+        return extraInfo;
+    }
+
+    public void setExtraInfo(String extraInfo) {
+        this.extraInfo = extraInfo;
+    }
+
+    public LocalTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalTime date) {
+        this.date = date;
+    }
+
+    public LocalTime getDoors() {
+        return doors;
+    }
+
+    public void setDoors(LocalTime doors) {
+        this.doors = doors;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
